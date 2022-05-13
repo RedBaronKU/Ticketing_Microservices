3e72f5f939c8a6713a489df692e9af7cd4397caa
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-13 14:31:49 +0530

    validation request middle ware added

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
+import { User } from "../models/user";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi There!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password must not be empty"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    res.send("Hi There!");
+  }
+);
 
 export { router as signinRouter };
