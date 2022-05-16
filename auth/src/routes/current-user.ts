3d8239c9ac1d696117899b78bd4b75ca4bf717cd
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-16 09:45:32 +0530

    currentUser route complete

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentUser", (req, res) => {
-  res.send("hi there");
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+    return;
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+    return;
+  } catch (e) {
+    res.send({ currentUser: null });
+    return;
+  }
 });
 
 export { router as currentUserRouter };
