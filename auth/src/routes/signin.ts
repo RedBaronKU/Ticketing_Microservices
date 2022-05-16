cb42200c43e34f75b325bb6d116e0308267fd1c2
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-16 09:44:29 +0530

    SignIN complete

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { BadRequestError } from "../errors/bad-request-error";
 import { User } from "../models/user";
+import { Password } from "../services/password";
 import jwt from "jsonwebtoken";
 import { validateRequest } from "../middlewares/validate-request";
 
@@ -19,7 +20,37 @@ router.post(
   validateRequest,
   async (req: Request, res: Response) => {
     const { email, password } = req.body;
-    res.send("Hi There!");
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid UserName or Password");
+    }
+
+    // Generate JSON web token
+
+    const userJWT = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // Store it on session object
+
+    req.session = {
+      jwt: userJWT,
+    };
+
+    res.status(201).send(existingUser);
   }
 );
 
