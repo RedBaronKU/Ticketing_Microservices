ad08602431cdfd461c8ad2bce00dec5bdb26114d
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-18 14:30:59 +0530

    Updated ticket index and app ts files

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
-
-import { errorHandler, NotFoundError } from "@redbaron_utk/common/build";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@redbaron_utk/common/build";
 import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -14,6 +18,9 @@ app.use(
     secure: true,
   })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.get("*", async () => {
   throw new NotFoundError("Not Found");
---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import { requireAuth } from "@redbaron_utk/common/build";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than zero"),
+  ],
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
+
+export { router as createTicketRouter };
