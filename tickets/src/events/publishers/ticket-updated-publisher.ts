0231a1e904fb4a1535faba045b3adc78170230ea
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-20 14:41:13 +0530

    Updated ticket update publisher

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,9 @@
+import {
+  Publisher,
+  Subjects,
+  TicketUpdatedEvent,
+} from "@redbaron_utk/common/build";
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.ticketUpdated = Subjects.ticketUpdated;
+}
