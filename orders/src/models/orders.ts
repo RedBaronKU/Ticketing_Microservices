23c8b8994d8f7046027eed40b10aeac818505af4
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-20 17:04:27 +0530

    updated order model:
    Added ticket model & updated Order model

--- orders/src/models/orders.ts
+++ orders/src/models/orders.ts
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
-
+import { OrderStatus } from "@redbaron_utk/common/build";
+import { TicketDoc } from "./ticket";
 interface OrderAttrs {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
 
 interface OrderDoc {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
@@ -27,6 +28,8 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
     },
     expiresAt: {
       type: mongoose.Schema.Types.Date,
