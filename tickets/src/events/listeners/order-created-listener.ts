aa611269c275b49bdf256fcaca1c69942708a6d6
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-23 16:37:20 +0530

    Pre-npm push

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,30 @@ import {
   Subjects,
 } from "@redbaron_utk/common/build";
 import { queueGroupName } from "./queue-group-name";
-
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { natsWrapper } from "../../nats-wrapper";
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    //Find the ticket that order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+    new TicketUpdatedPublisher();
+
+    // ack the message
+    msg.ack();
+  }
 }
