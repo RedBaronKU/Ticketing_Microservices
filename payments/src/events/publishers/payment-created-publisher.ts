c1979bc78cf51c7e4234f6ec96e14be31a90fd31
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-24 11:32:10 +0530

    bugFix/payments

--- payments/src/events/publishers/payment-created-publisher.ts
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -1,4 +1,8 @@
-import { Subjects, Publisher, PaymentCreatedEvent } from '@cygnetops/common';
+import {
+  Subjects,
+  Publisher,
+  PaymentCreatedEvent,
+} from "@redbaron_utk/common/build";
 
 export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
   subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
