ef1555c4d8834e7846b3c3b23adf8d721f255345
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-24 11:49:42 +0530

    changes to common @v17

--- common/build/events/base-listener.js
+++ common/build/events/base-listener.js
@@ -16,17 +16,17 @@ class Listener {
     }
     listen() {
         const subscription = this.client.subscribe(this.subject, this.queueGroupName, this.subscriptionOptions());
-        subscription.on('message', (msg) => {
-            console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+        subscription.on("message", (msg) => {
+            console.log(`Message Received: ${this.subject} / ${this.queueGroupName}`);
             const parsedData = this.parseMessage(msg);
             this.onMessage(parsedData, msg);
         });
     }
     parseMessage(msg) {
         const data = msg.getData();
-        return typeof data === 'string'
+        return typeof data === "string"
             ? JSON.parse(data)
-            : JSON.parse(data.toString('utf8'));
+            : JSON.parse(data.toString("utf8"));
     }
 }
 exports.Listener = Listener;
