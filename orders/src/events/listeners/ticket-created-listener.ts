13ace9052aa16060c634652effcb915f3a3323cc
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-24 10:50:17 +0530

    Adding expiration listener event on order

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -8,7 +8,7 @@ import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.ticketCreated = Subjects.ticketCreated;
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
