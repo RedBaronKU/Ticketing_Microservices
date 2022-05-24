0a3cb13b0b7fd31f3c54951a194ab709f945a094
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-05-24 10:20:19 +0530

    bugfix/expiration

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -2,7 +2,7 @@ import {
   Subjects,
   Publisher,
   ExpirationCompleteEvent,
-} from '@cygnetops/common';
+} from "@redbaron_utk/common/build";
 
 export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
